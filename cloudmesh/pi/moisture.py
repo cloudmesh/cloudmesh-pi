2efba527071957e23120de441c7110c6fc8797ed
Jon <jon@ISELAB-MAC-14.local>
2017-07-28 14:53:13 -0400

    new:dev: new grove modules

---
+++ cloudmesh/pi/moisture.py
@@ -0,0 +1,23 @@
+# 	Sensor values observer:
+# 		Val  Condition
+# 		0    sensor in open air
+# 		18   sensor in dry soil
+# 		425  sensor in humid soil
+# 		690  sensor in water
+
+import time
+import grovepi
+
+# Connect the Grove Moisture Sensor to analog port A0
+# SIG,NC,VCC,GND
+class MoistureSensor(object):
+
+    def __init__(self, pin=0):
+        """
+        Connect sensor to analog
+        :param pin: Number
+        """
+        self.pin = pin
+
+    def get(self):
+        return grovepi.analogRead(self.pin)
