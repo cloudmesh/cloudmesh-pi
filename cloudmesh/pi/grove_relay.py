f833cfb10beec78d1de6330332093cd92f0c64d5
Arnav Arnav <aarnav@iu.edu>
2017-10-24 02:34:29 +0000

    added class for grove_relay

---
+++ cloudmesh/pi/grove_relay.py
@@ -0,0 +1,33 @@
+import grovepi
+import time
+
+class GroveRelay:
+	def __init__(self, pin = 3):
+		"""
+		set the pin for relay
+		by default pin 3
+		"""
+		self.relay = pin
+		grovepi.pinMode(self.relay,"OUTPUT")
+		return
+
+	def on(self):
+		"""
+		turn on the relay allowing the current to flow through the connected device
+		"""
+		grovepi.digitalWrite(self.relay, 1)
+		return
+
+	def off(self):
+		"""
+		tunrn off the relay preventing current from flowing to the connected device
+		"""
+		grovepi.digitalWrite(self.relay, 0)
+		return
+
+
+if __name__ == "__main__":
+	relay = GroveRelay(4)
+	relay.on()
+	time.sleep(5)
+	relay.off()
