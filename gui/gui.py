8099839196c4829ae95e2f2b7115db983a51518f
Gregor von Laszewski <laszewski@gmail.com>
2017-11-30 18:02:50 -0500

    fixing code with STEM students

--- gui/gui.py
+++ gui/gui.py
@@ -1,40 +1,66 @@
-from guizero import App, Text, Box, PushButton
+from guizero import App, Text, Box, PushButton, Picture
 import datetime
 from time import gmtime, strftime
 import os
 font_size = 12
 
 # create global variables.
-now = "measure"
-temperature = "measure"
+now = "..."
+temperature = "..."
+turbidity = "..."
+sun = "..."
+chop = "..."
+observer = "starboat"
+gps = "..."
+#wind
+
+counter = 1
 
 filename = os.path.expanduser("~/data.log")
 
 def do_measure():
-    global now, temperature
+
+    global now, temperature, turbidity, sun, chop, observer, gps
+
+    global now, temperature, counter
+    counter = counter + 1
+    now = datetime.datetime.now().strftime('%Y-%m-%d, %H:%M:%S')
+
     # get values from pi
     # update values to global vars (time, temperature)
-    now = datetime.datetime.now().strftime('%Y-%m-%d, %H:%M:%S')
-    
-    entry("Time:", now, 0,1)
-    print ("Time", now)
+
+    entry("Counter:", counter, 0,1)
+    entry("Time:", now, 0,2)
+    entry("Temperature:", temperature, 0, 3)
+    entry("Turbidity:", turbidity, 0, 4)
+    entry("Sun:", sun, 0, 5)
+    entry("Chop:", chop, 0, 6)
+    entry("GPS:", gps, 0, 7)
+    entry("Observer:", observer, 0, 8)
 
     with open(filename, "a") as myfile:
         myfile.write(now + ", " + temperature + "\n")
-
     
     return 0
 
 def entry(label, value, x, y):
     l = Text(app, text=label,  size=font_size, align="left",  grid=[y,x])
     t = Text(app, text=value, size=font_size, align="left", grid=[y, x+1])
+    print (label, value)
     
 
 app = App(title="My Application", height=300, width=400, layout="grid")
 
-t = "0"
-entry("Time:", now, 0,1)
-entry("Temperature:", "temp", 0, 2)
+# picture = Picture(app, image="logo.gif", grid=[0,0])
+
+entry("Counter:", counter, 0,1)
+entry("Time:", now, 0,2)
+entry("Temperature:", temperature, 0, 3)
+entry("Turbidity:", turbidity, 0, 4)
+entry("Sun:", sun, 0, 5)
+entry("Chop:", chop, 0, 6)
+entry("GPS:", gps, 0, 7)
+entry("Observer:", observer, 0, 8)
 
 button = PushButton(app, command=do_measure, text="measure", grid=[0,0])
 app.display()
