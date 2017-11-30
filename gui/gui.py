f65a5673af53750f1ef8f0223aaa220df5db6863
Gregor von Laszewski <laszewski@gmail.com>
2017-11-30 12:26:31 -0500

    update example for loggng data

--- gui/gui.py
+++ gui/gui.py
@@ -1,27 +1,33 @@
 from guizero import App, Text, Box, PushButton
 import datetime
 from time import gmtime, strftime
+import os
+font_size = 12
 
 # create global variables.
 now = "measure"
 temperature = "measure"
 
+filename = os.path.expanduser("~/data.log")
 
 def do_measure():
-    global now
+    global now, temperature
     # get values from pi
     # update values to global vars (time, temperature)
-    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    now = datetime.datetime.now().strftime('%Y-%m-%d, %H:%M:%S')
     
     entry("Time:", now, 0,1)
     print ("Time", now)
+
+    with open(filename, "a") as myfile:
+        myfile.write(now + ", " + temperature + "\n")
 
     
     return 0
 
 def entry(label, value, x, y):
-    l = Text(app, text=label,  align="left",  grid=[y,x])
-    t = Text(app, text=value, align="left", grid=[y, x+1])
+    l = Text(app, text=label,  size=font_size, align="left",  grid=[y,x])
+    t = Text(app, text=value, size=font_size, align="left", grid=[y, x+1])
     
 
 app = App(title="My Application", height=300, width=400, layout="grid")
