46f5322a70a882692e81f0db6a1657c2aaa09d68
fugangwang <kevinwangfg@gmail.com>
2018-04-19 16:07:58 -0400

    adding scaled reading from joystick; filter out noise values in the boatcontroller

--- cloudmesh/pi/boatcontroller.py
+++ cloudmesh/pi/boatcontroller.py
@@ -9,14 +9,20 @@ def main():
     MQTT_PATH = "boatcontrol"
 
     stick = Joystick()
+    prevxy = [0, 0]
     while True:
         curTime = datetime.datetime.now()
-        (x,y,z) = stick.get()
+        (x,y,z) = stick.get_scaled()
         #print ("%s|%s|%s" % (x,y,z))
         if int(x)|int(y)|int(z) != 0:
             pubstr = "%s: %s,%s,%s" % (curTime, x, y, z)
             print (pubstr)
-            publish.single(MQTT_PATH, pubstr, hostname=MQTT_SERVER)
+            # filter out abrupt/false click probably due to EMI
+            # only publish the REAL ones if we found two consecutive valid coordinate changes within 0.1 second
+            if x*prevxy[0] != 0 or y*prevxy[1] != 0:
+                publish.single(MQTT_PATH, pubstr, hostname=MQTT_SERVER)
+        prevxy[0] = x
+        prevxy[1] = y
         time.sleep(0.1)
 
 if __name__ == "__main__":
--- cloudmesh/pi/joystick.py
+++ cloudmesh/pi/joystick.py
@@ -1,5 +1,6 @@
 import os
 import time
+import datetime
 import grovepi
 
 class Joystick(object):
@@ -35,13 +36,13 @@ class Joystick(object):
         grovepi.pinMode(self.yPin, "INPUT")
         if calibrate:
             self.calibrate()
-        config = self.read_calibrate()
-        self.max_y = config["ymax"]
-        self.min_y = config["ymin"]
-        self.max_x = config["xmax"]
-        self.min_x = config["xmin"]
-        self.typical_x = config["x0"]
-        self.typical_y = config["y0"]
+        self.config = self.read_calibrate()
+        self.max_y = self.config["ymax"]
+        self.min_y = self.config["ymin"]
+        self.max_x = self.config["xmax"]
+        self.min_x = self.config["xmin"]
+        self.x0 = self.config["x0"]
+        self.y0 = self.config["y0"]
 
     def calibrate(self):
         config = self.read_calibrate()
@@ -151,6 +152,21 @@ class Joystick(object):
             print ("IOError")
         return (x,y)
 
+    # x,y values normalized in the range of [-100, 100]
+    def get_scaled(self):
+        (x,y,z) = self.get()
+        if x > 0:
+            x = x * 100/(self.max_x - self.x0)
+        else:
+            if x < 0:
+                x = x * 100 / (self.x0 - self.min_x)
+        if y > 0:
+            y = y * 100/(self.max_y - self.y0)
+        else:
+            if y < 0:
+                y = y  * 100 / (self.y0 - self.min_y)
+        return (x,y,z)
+
     def get(self):
         """
         gets the x, y, and click status of the joystick
@@ -162,16 +178,20 @@ class Joystick(object):
             y = grovepi.analogRead(self.yPin)
 
             # Was a click detected on the X axis?
-            click = 1 if x >= 1020 else 0
+            click = 1 if x >= self.config["click"] else 0
 
             #print("x =", x, " y =", y, " click =", click)
-            x = x - self.typical_x
-            y = y - self.typical_y
+            x = x - self.x0
+            y = y - self.y0
 
             if abs(x) <= 1:
                 x = 0
             if abs(y) <= 1:
                 y = 0
+
+            # when click, x value no meaning at all so set to zero
+            if click == 1:
+                x = 0
             return x, y, click
 
         except IOError:
@@ -190,15 +210,15 @@ class Joystick(object):
             y = grovepi.analogRead(self.yPin)
 
             # Was a click detected on the X axis?
-            click = 1 if x >= 1020 else 0
+            click = 1 if x >= self.config["click"] else 0
 
             print("x =", x, " y =", y, " click =", click)
             return x - self.min_x, y - self.min_y, click
 
         except IOError:
             print ("Error")
-            x = self.typical_x - self.min_x
-            y = self.typical_y - self.min_y
+            x = self.x0 - self.min_x
+            y = self.y0 - self.min_y
 
 
 
@@ -206,8 +226,8 @@ if __name__ == "__main__":
     # call with calibrate
     stick = Joystick(calibrate=True)
     #print(stick.get_absolute())
-    print(stick.get())
+    print(stick.get_scaled())
     # assuming calibrate had been done
     stick = Joystick()
     #print(stick.get_absolute())
-    print(stick.get())
+    print(stick.get_scaled())
