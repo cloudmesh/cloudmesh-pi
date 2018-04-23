ef99add540ddfb258aed677b3c85579f06a46849
Gregor von Laszewski <laszewski@gmail.com>
2018-04-23 14:43:01 -0400

    simple commandline test

---
+++ examples/turbidity_cli.py
@@ -0,0 +1,21 @@
+######################################################################
+# This program reads analog turbidity sensor values and
+# displays it on the lcd screen
+#
+# Connect the turbidity sensor to port A0 of the grovepi
+# Connect LCD to an I2C port of grovepi
+#
+# Question: can we use any I2C and any analog port.
+# Is there a combination the would not work
+#
+######################################################################
+from __futures__ import print_function
+from cloudmesh.pi import TurbiditySensorAnalog
+import time
+
+turbidity = TurbiditySensorAnalog(pin=0)
+
+while True:
+    value = turbidity.getValue()
+    print("Turbidity : " + str(value))
+    time.sleep(1)
